16b1705919cb4bc6ec7855086f257a2afdba96b8
wangyifu <wangyifu@t4f-mbp-13717.local>
2014-06-14 16:40:26 +0800

    Make a basic User model (including secure passwords)

---
+++ db/migrate/20140614072854_change_password_name_to_users.rb
@@ -0,0 +1,5 @@
+class ChangePasswordNameToUsers < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
---
+++ db/migrate/20140614083453_remove_password_confirmation_to_users.rb
@@ -0,0 +1,5 @@
+class RemovePasswordConfirmationToUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :password_confirmation
+  end
+end
